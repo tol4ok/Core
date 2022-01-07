60eb2c128fdf4284a253f591fba13028b8b15750
tol4ok <73625734+tol4ok@users.noreply.github.com>
2022-01-07 17:01:23 +1200

    Создание миров при запуске сервера

--- src/main/java/me/kuboeb69/core/Core.java
+++ src/main/java/me/kuboeb69/core/Core.java
@@ -4,13 +4,20 @@ import me.kuboeb69.core.commands.CommandManager;
 import me.kuboeb69.core.tabcompleters.WorldTabCompleter;
 
 import org.bukkit.Server;
+import org.bukkit.WorldCreator;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Core extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        FileConfiguration config = getConfig();
         Server server = getServer();
+
+        for(String world_name : config.getStringList("worlds")) {
+            getServer().createWorld(new WorldCreator(world_name));
+        }
 
         server.getPluginCommand("world").setExecutor(new CommandManager(this));
         server.getPluginCommand("world").setTabCompleter(new WorldTabCompleter(this));
